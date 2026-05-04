a8e857a2b7e8233218571f3a129fda9954e92c0a
Joao Marcos <jmarcos.yb@gmail.com>
2026-05-04 14:55:16 -0300

    refactor: changing extensions name and editing pages

--- assets/js/footerPT.js
+++ assets/js/footerPT.js
@@ -1,49 +1,53 @@
 const footerElement = document.querySelector('.site-footer');
 
 const footerHTML = `
-  <footer id="contato" class="site-footer">
-
-    <div class="footer-main">
+  <div class="footer-main">
 
       <div class="footer-left-column">
 
-        <div class="footer-col contact-info">
-          <a href="https://www.google.com/maps/place/Janu%C3%A1ria,+MG" class="location-link" target="_blank">
-            <div class="location-text">
-              <p>Januária - MG</p>
-              <p>Rua Padre Henrique - Centro</p>
-            </div>
-            <img src="../../assets/img/Social Icons/ponteiro.png" alt="Icone de localização" class="location-icon">
-          </a>
-          <br>
-          <p>(11) 4002-8922</p>
-          <p>turismo@guiajanu.com</p>
+        <div class="footer-col links-col">
+          <h4>Email Para Contato</h4>
+          <ul>
+            <p>turismo@guiajanu.com</p>
+            </ul>
         </div>
 
         <div class="footer-col links-col">
-          <h4>O que fazer</h4>
+          <h4>Conheça Nosso App</h4>
           <ul>
-            <li><a href="#">Como Chegar</a></li>
-            <li><a href="#">Atendimento ao Turista</a></li>
-            <li><a href="#">Calendário de Eventos</a></li>
-            <li><a href="#">Meios de Transporte</a></li>
-          </ul>
+            <li><a href="../pages/emconstrução.html">GuiaJanu Autoguiado</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-col links-col">
+          <h4>Descubra Januária</h4>
+          <ul>
+            <li><a href="../pages/ACidade.hmtl">A Cidade</a></li>
+            <li><a href="../pages/comochegar.hmtl">Como Chegar</a></li>
+            </ul>
         </div>
 
       </div>
 
       <div class="footer-right-column">
         <div class="footer-col links-col">
-          <h4>Descubra Januária</h4>
+          <h4>O que fazer</h4>
           <ul>
-            <li><a href="#">Passeios</a></li>
-            <li><a href="#">Praia</a></li>
-            <li><a href="#">Praças</a></li>
-            <li><a href="#">Hospedagem</a></li>
-            <li><a href="#">Igrejas</a></li>
-            <li><a href="#">Mirante</a></li>
-            <li><a href="#">Rota da Cachaça</a></li>
-            <li><a href="#">Feira</a></li>
+            <li><a href="../pages/ListagemPontosTuristicos.html">Listagem Pontos Turisticos</a></li>
+            <li><a href="#">Em Construção</a></li>
+          </ul>
+        </div>
+        <div class="footer-col links-col">
+          <h4>Onde Ficar</h4>
+          <ul>
+            <li><a href="#">Em Construção</a></li>
+          </ul>
+        </div>
+        <div class="footer-col links-col">
+          <h4>Mais</h4>
+          <ul>
+            <li><a href="#">CAT</a></li>
+            <li><a href="#">Sobre Nós</a></li>
           </ul>
         </div>
       </div>
@@ -51,7 +55,7 @@ const footerHTML = `
     </div>
 
     <div class="footer-bottom">
-      <p>&copy; 2025 GuiaJanu - Todos os direitos reservados.</p>
+      <p>&copy; 2026 GuiaJanu - Todos os direitos reservados.</p>
       
       <p>Desenvolvido por:
         <a href="https://www.instagram.com/eric_lopes01" target="_blank">Eric L. Guimarães</a>,
@@ -71,7 +75,7 @@ const footerHTML = `
             <div class="logo-item">
               <a href="https://www.ifnmg.edu.br/januaria" target="_blank" class="logo-link">
                 <p>IFNMG</p>
-                <img src="../../assets/img/Logo/IFNMG3.png" alt="Logo IFNMG Campus Januária" class="partner-logo">
+                <img src="../assets/img/Logo/IFNMG3.png" alt="Logo IFNMG Campus Januária" class="partner-logo">
                 <p class="ifnmg">
                   <span class="linha1">INSTITUTO FEDERAL</span>
                   <span class="linha2">Norte de Minas Gerais</span>
@@ -83,14 +87,14 @@ const footerHTML = `
             <div class="logo-item">
               <a href="https://www.instagram.com/guia.janu" target="_blank" class="logo-link">
                 <p>UCE-BSI</p>
-                <img src="../../assets/img/Social Icons/insta_logo.png" alt="Logo UCE-BSI" class="partner-logo logo-insta">
+                <img src="../assets/img/Social Icons/insta_logo.png" alt="Logo UCE-BSI" class="partner-logo logo-insta">
               </a>
             </div>
             
             <div class="logo-item">
               <a href="https://www.instagram.com/petadmifnmg" target="_blank" class="logo-link">
                 <p>PET-ADM</p>
-                <img src="../../assets/img/Parcerias/PETADM.png" alt="Logo PET-ADM" class="partner-logo">
+                <img src="../assets/img/Parcerias/PETADM.png" alt="Logo PET-ADM" class="partner-logo">
               </a>
             </div>
             
@@ -104,21 +108,21 @@ const footerHTML = `
             <div class="logo-item">
               <a href="https://www.januaria.mg.gov.br/portal/secretarias/26/setur--secretaria-municipal-de-turismo-cultura-meio-ambiente-e-desenvolvimento-economico" target="_blank" class="logo-link">
                 <p>SETUR</p>
-                <img src="../../assets/img/Parcerias/SETUR.png" alt="Setur" class="partner-logo">
+                <img src="../assets/img/Parcerias/SETUR.png" alt="Setur" class="partner-logo">
               </a>
             </div>
             
             <div class="logo-item">
               <a href="https://www.instagram.com/la_belle_janu" target="_blank" class="logo-link">
                 <p>La Belle Janu</p>
-                <img src="../../assets/img/Social Icons/insta_logo.png" alt="La Belle Janu" class="partner-logo logo-insta">
+                <img src="../assets/img/Social Icons/insta_logo.png" alt="La Belle Janu" class="partner-logo logo-insta">
               </a>
             </div>
             
             <div class="logo-item">
               <a href="https://www.youtube.com/c/SERTÕESGERAIS" target="_blank" class="logo-link">
                 <p>Canal Sertões Gerais</p>
-                <img src="../../assets/img/Parcerias/SERTOESGERAIS.png" alt="Canal Sertão Mineiro" class="partner-logo">
+                <img src="../assets/img/Parcerias/SERTOESGERAIS.png" alt="Canal Sertão Mineiro" class="partner-logo">
               </a>
             </div>
             
@@ -130,8 +134,6 @@ const footerHTML = `
       <p>As imagens nesse site são de inteira responsabilidade dos proprietários dos estabelecimentos cadastrados em nosso banco de dados.</p>
     
     </div>
-
-  </footer>
 `;
 
 if (footerElement) {
