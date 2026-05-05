b6d8f2ba98d61aecbac7da57eb428cbaf6fc6bb2
Eric Lopes <elmg@aluno.infmg.edu.br>
2026-05-05 16:06:59 -0300

    fix: arrumando o site em construção

--- assets/js/footerIN.js
+++ assets/js/footerIN.js
@@ -35,26 +35,26 @@ const footerHTML = `
         <div class="footer-col links-col">
           <h4>O que fazer</h4>
           <ul>
-            <li><a href="../pages/ListagemPontosTuristicos.html">Listagem Pontos Turisticos</a></li>
+            <li><a href="./pages/ListagemPontosTuristicos.html">Listagem Pontos Turisticos</a></li>
           </ul>
         </div>
         <div class="footer-col links-col">
           <h4>Onde Comer</h4>
           <ul>
-            <li><a href="../pages/emconstrucao.html">Em Construção</a></li>
+            <li><a href="./pages/emconstrucao.html">Em Construção</a></li>
           </ul>
         </div>
         <div class="footer-col links-col">
           <h4>Onde Ficar</h4>
           <ul>
-            <li><a href="../pages/emconstrucao.html">Em Construção</a></li>
+            <li><a href="./pages/emconstrucao.html">Em Construção</a></li>
           </ul>
         </div>
         <div class="footer-col links-col">
           <h4>Mais</h4>
           <ul>
-            <li><a href="../pages/cat.html">CAT</a></li>
-            <li><a href="../pages/aboutUs.html">Sobre Nós</a></li>
+            <li><a href="./pages/cat.html">CAT</a></li>
+            <li><a href="./pages/aboutUs.html">Sobre Nós</a></li>
           </ul>
         </div>
       </div>
@@ -62,7 +62,7 @@ const footerHTML = `
     </div>
 
     <div class="footer-bottom">
-      <p>&copy; 2025 GuiaJanu - Todos os direitos reservados.</p>
+      <p>&copy; 2026 GuiaJanu - Todos os direitos reservados.</p>
       
       <p>Desenvolvido por:
         <a href="https://www.instagram.com/eric_lopes01" target="_blank">Eric L. Guimarães</a>,
